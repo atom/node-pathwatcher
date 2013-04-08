1cb3ba9d392ec1938cbbbddf33a641a3c96fdb50
Cheng Zhao <zcbenz@gmail.com>
2013-04-08 11:49:46 +0800

    handleWatchers is a map not an array.

--- test.js
+++
@@ -1,7 +0,0 @@
-watcher = require('./lib/main.js');
-
-var r = watcher.watch('/tmp/test.txt', function(event, path) {
-  console.log(event, path);
-});
-
-require('fs').writeFileSync('/tmp/test.txt', '1');
