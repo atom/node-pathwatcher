0a5519a3fbf384fda2f18c75f438cafea8b8ce3c
Paul Betts <paul@paulbetts.org>
2013-10-30 11:39:35 -0700

    Indentation fixes

--- src/pathwatcher_win.cc
+++ src/pathwatcher_win.cc
@@ -53,14 +53,14 @@ struct HandleWrapper {
   static std::map<WatcherHandle, HandleWrapper*> map_;
 };
 
-std::map<WatcherHandle, HandleWrapper*> HandleWrapper::map_;
-
 struct WatcherEvent {
   EVENT_TYPE type;
   WatcherHandle handle;
   std::vector<char> new_path;
   std::vector<char> old_path;
 };
+
+std::map<WatcherHandle, HandleWrapper*> HandleWrapper::map_;
 
 static bool QueueReaddirchanges(HandleWrapper* handle) {
   return ReadDirectoryChangesW(handle->dir_handle,
@@ -122,110 +122,113 @@ void PlatformThread() {
                                      FALSE,
                                      INFINITE);
     int i = r - WAIT_OBJECT_0;
-    if (i >= 0 && i < copied_events.size()) {
-      if (g_events[i] == g_wake_up_event)
-        continue;
 
-      std::vector<char> old_path;
-      std::vector<WatcherEvent> events;
+    if (i < 0 || i >= copied_events.size())
+      continue;
 
-      uv_mutex_lock(&g_handle_wrap_map_mutex);
-      HandleWrapper* handle = HandleWrapper::Get(copied_events[i]);
-      if (!handle) {
-        uv_mutex_unlock(&g_handle_wrap_map_mutex);
-        continue;
-      }
+    if (g_events[i] == g_wake_up_event)
+      continue;
 
-      if (handle->canceled) {
-        delete handle;
-        uv_mutex_unlock(&g_handle_wrap_map_mutex);
-        continue;
-      }
+    std::vector<char> old_path;
+    std::vector<WatcherEvent> events;
 
-      DWORD bytes;
-      GetOverlappedResult(handle->dir_handle,
-                          &handle->overlapped,
-                          &bytes,
-                          FALSE);
-
-      DWORD offset = 0;
-      do {
-        FILE_NOTIFY_INFORMATION* file_info =
-            reinterpret_cast<FILE_NOTIFY_INFORMATION*>(handle->buffer + offset);
-
-        // Emit events for children.
-        EVENT_TYPE event = EVENT_NONE;
-        switch (file_info->Action) {
-          case FILE_ACTION_ADDED:
-            event = EVENT_CHILD_CREATE;
-            break;
-          case FILE_ACTION_REMOVED:
-            event = EVENT_CHILD_DELETE;
-            break;
-          case FILE_ACTION_RENAMED_OLD_NAME:
-            event = EVENT_CHILD_RENAME;
-            break;
-          case FILE_ACTION_RENAMED_NEW_NAME:
-            event = EVENT_CHILD_RENAME;
-            break;
-          case FILE_ACTION_MODIFIED:
-            event = EVENT_CHILD_CHANGE;
-            break;
-        }
-
-        if (event != EVENT_NONE) {
-          // The FileNameLength is in "bytes", but the WideCharToMultiByte
-          // requires the length to be in "characters"!
-          int file_name_length_in_characters =
-              file_info->FileNameLength / sizeof(wchar_t);
-
-          char filename[MAX_PATH] = { 0 };
-          int size = WideCharToMultiByte(CP_UTF8,
-                                         0,
-                                         file_info->FileName,
-                                         file_name_length_in_characters,
-                                         filename,
-                                         MAX_PATH,
-                                         NULL,
-                                         NULL);
-
-          // Convert file name to file path, same with:
-          // path = handle->path + '\\' + filename
-          std::vector<char> path(handle->path.size() + 1 + size);
-          std::vector<char>::iterator iter = path.begin();
-          iter = std::copy(handle->path.begin(), handle->path.end(), iter);
-          *(iter++) = '\\';
-          std::copy(filename, filename + size, iter);
-
-          if (file_info->Action == FILE_ACTION_RENAMED_OLD_NAME) {
-            // Do not send rename event until the NEW_NAME event, but still keep
-            // a record of old name.
-            old_path.swap(path);
-          } else if (file_info->Action == FILE_ACTION_RENAMED_NEW_NAME) {
-            WatcherEvent e = { event, handle->overlapped.hEvent };
-            e.new_path.swap(path);
-            e.old_path.swap(old_path);
-            events.push_back(e);
-          } else {
-            WatcherEvent e = { event, handle->overlapped.hEvent };
-            e.new_path.swap(path);
-            events.push_back(e);
-          }
-        }
-
-        offset = file_info->NextEntryOffset;
-      } while (offset);
-
-      // Restart the monitor, it was reset after each call.
-      QueueReaddirchanges(handle);
-
+    uv_mutex_lock(&g_handle_wrap_map_mutex);
+    HandleWrapper* handle = HandleWrapper::Get(copied_events[i]);
+    if (!handle) {
       uv_mutex_unlock(&g_handle_wrap_map_mutex);
+      continue;
+    }
 
-      for (int i = 0; i < events.size(); ++i)
-        PostEventAndWait(events[i].type,
-                         events[i].handle,
-                         events[i].new_path,
-                         events[i].old_path);
+    if (handle->canceled) {
+      delete handle;
+      uv_mutex_unlock(&g_handle_wrap_map_mutex);
+      continue;
+    }
+
+    DWORD bytes;
+    GetOverlappedResult(handle->dir_handle,
+                        &handle->overlapped,
+                        &bytes,
+                        FALSE);
+
+    DWORD offset = 0;
+    do {
+      FILE_NOTIFY_INFORMATION* file_info =
+          reinterpret_cast<FILE_NOTIFY_INFORMATION*>(handle->buffer + offset);
+
+      // Emit events for children.
+      EVENT_TYPE event = EVENT_NONE;
+      switch (file_info->Action) {
+        case FILE_ACTION_ADDED:
+          event = EVENT_CHILD_CREATE;
+          break;
+        case FILE_ACTION_REMOVED:
+          event = EVENT_CHILD_DELETE;
+          break;
+        case FILE_ACTION_RENAMED_OLD_NAME:
+          event = EVENT_CHILD_RENAME;
+          break;
+        case FILE_ACTION_RENAMED_NEW_NAME:
+          event = EVENT_CHILD_RENAME;
+          break;
+        case FILE_ACTION_MODIFIED:
+          event = EVENT_CHILD_CHANGE;
+          break;
+      }
+
+      if (event != EVENT_NONE) {
+        // The FileNameLength is in "bytes", but the WideCharToMultiByte
+        // requires the length to be in "characters"!
+        int file_name_length_in_characters =
+            file_info->FileNameLength / sizeof(wchar_t);
+
+        char filename[MAX_PATH] = { 0 };
+        int size = WideCharToMultiByte(CP_UTF8,
+                                       0,
+                                       file_info->FileName,
+                                       file_name_length_in_characters,
+                                       filename,
+                                       MAX_PATH,
+                                       NULL,
+                                       NULL);
+
+        // Convert file name to file path, same with:
+        // path = handle->path + '\\' + filename
+        std::vector<char> path(handle->path.size() + 1 + size);
+        std::vector<char>::iterator iter = path.begin();
+        iter = std::copy(handle->path.begin(), handle->path.end(), iter);
+        *(iter++) = '\\';
+        std::copy(filename, filename + size, iter);
+
+        if (file_info->Action == FILE_ACTION_RENAMED_OLD_NAME) {
+          // Do not send rename event until the NEW_NAME event, but still keep
+          // a record of old name.
+          old_path.swap(path);
+        } else if (file_info->Action == FILE_ACTION_RENAMED_NEW_NAME) {
+          WatcherEvent e = { event, handle->overlapped.hEvent };
+          e.new_path.swap(path);
+          e.old_path.swap(old_path);
+          events.push_back(e);
+        } else {
+          WatcherEvent e = { event, handle->overlapped.hEvent };
+          e.new_path.swap(path);
+          events.push_back(e);
+        }
+      }
+
+      offset = file_info->NextEntryOffset;
+    } while (offset);
+
+    // Restart the monitor, it was reset after each call.
+    QueueReaddirchanges(handle);
+
+    uv_mutex_unlock(&g_handle_wrap_map_mutex);
+
+    for (int i = 0; i < events.size(); ++i) {
+      PostEventAndWait(events[i].type,
+                       events[i].handle,
+                       events[i].new_path,
+                       events[i].old_path);
     }
   }
 }
@@ -250,6 +253,7 @@ WatcherHandle PlatformWatch(const char* path) {
                                      FILE_FLAG_BACKUP_SEMANTICS |
                                        FILE_FLAG_OVERLAPPED,
                                      NULL);
+
   if (!PlatformIsHandleValid(handle)) {
     fprintf(stderr, "Unable to call CreateFileW for %s\n", path);
     return INVALID_HANDLE_VALUE;
